ebda93763cf001e3fea488d791fc5cc605dd2fa7
Boring Euro Dev <245870614+BoringEuropeanDev@users.noreply.github.com>
2026-03-19 13:21:21 +0100

    Update route.ts

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,6 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
-import { createServerClient } from '@/lib/supabase';
+import { createRouteHandlerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+const supabase = createRouteHandlerClient({ cookies() })
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
